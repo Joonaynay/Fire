ff41043827f1a320c210b0b5a01b778216a90cdf
Joonaynay <Joonaynay@gmail.com>
2021-08-20 23:03:36 -0600

    Initial Commit

---
+++ Fire/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fire
+//
+//  Created by Forrest Buhler on 8/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fire/FireApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireApp.swift
+//  Fire
+//
+//  Created by Forrest Buhler on 8/20/21.
+//
+
+import SwiftUI
+
+@main
+struct FireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
